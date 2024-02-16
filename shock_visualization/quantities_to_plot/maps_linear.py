a5430c429594a0cf91ff230f033f736878b4062e
Karol Fulat <karol.fulat@uni-potsdam.de>
2024-02-16 09:58:19 +0100

    Add current densities of ions and electrons

--- shock_visualization/quantities_to_plot/maps_linear.py
+++ shock_visualization/quantities_to_plot/maps_linear.py
@@ -1,11 +1,11 @@
 from copy import deepcopy
 
 from shock_visualization.constants import (
-    PATH_TO_RESULT, PATH_TO_PLOT, N0, b0, e0, B0, E0
+    PATH_TO_RESULT, PATH_TO_PLOT, N0, b0, e0, B0, E0, V0, C
 )
 from shock_visualization.quantities import Density, Field, PlotParams
 
-filter_sigma = 0.5
+filter_sigma = 1.0
 
 Ni = Density(PATH_TO_RESULT, "densiresR", N0, filter_sigma, 0.0, False, True, False)
 Ne = Density(PATH_TO_RESULT, "denseresR", N0, filter_sigma, 0.0, False, True, False)
@@ -16,14 +16,24 @@ Ex = Field(PATH_TO_RESULT, "exres", e0, filter_sigma, E0[0], False, True, True)
 Ey = Field(PATH_TO_RESULT, "eyres", e0, filter_sigma, E0[1], False, True, True)
 Ez = Field(PATH_TO_RESULT, "ezres", e0, filter_sigma, E0[2], False, True, True)
 
+Jix = Field(PATH_TO_RESULT, "currxiresR", N0*V0, filter_sigma, N0*V0, False, True, True)
+Jiy = Field(PATH_TO_RESULT, "curryiresR", N0*V0, filter_sigma, 0.0, False, True, False)
+Jiz = Field(PATH_TO_RESULT, "currziresR", N0*V0, filter_sigma, 0.0, False, True, False)
+
+Jex = Field(PATH_TO_RESULT, "currxeresR", N0*V0, filter_sigma, N0*V0, False, True, True)
+Jey = Field(PATH_TO_RESULT, "curryeresR", N0*V0, filter_sigma, 0.0, False, True, False)
+Jez = Field(PATH_TO_RESULT, "currzeresR", N0*V0, filter_sigma, 0.0, False, True, False)
+
 # Common plot parameters for all maps
-lims = [(0, 100), (None, None)]
+lims = [(0, 5.75), (0, 5.75)]
 labels = [r"$x/\lambda_{si}$", r"$y/\lambda_{si}$"]
-major_loc = (10.0,2.0)
-minor_loc = (1.0,1.0)
-colspan = 20
+major_loc = (1.0,1.0)
+minor_loc = (0.2,0.2)
+colspan = 1
 fig_name = "step"
 cmap = "turbo"
+cbar_size = "5%"
+cbar_pad = "5%"
 
 # Initialize plot parameters for density and field maps
 dens_plot_params = PlotParams(
@@ -38,8 +48,10 @@ dens_plot_params = PlotParams(
     hide_ylabels = False,
     major_loc = major_loc,
     minor_loc = minor_loc,
-    levels = (1.0-0.05, 1.0+0.05),
+    levels = (1.0-0.15, 1.0+0.15),
     cbar_label = "",
+    cbar_size=cbar_size,
+    cbar_pad=cbar_pad,
 )
 
 bfield_plot_params = PlotParams(
@@ -57,6 +69,8 @@ bfield_plot_params = PlotParams(
     levels = (-1e-2, 1e-2),
     # levels = (None, None),
     cbar_label = "",
+    cbar_size=cbar_size,
+    cbar_pad=cbar_pad,
 )
 
 efield_plot_params = PlotParams(
@@ -74,6 +88,26 @@ efield_plot_params = PlotParams(
     levels = (-0.2, 0.2),
     # levels = (None, None),
     cbar_label = "",
+    cbar_size=cbar_size,
+    cbar_pad=cbar_pad,
+)
+
+curr_plot_params = PlotParams(
+    colspan = colspan,
+    fig_name = "step_ion",
+    plot_path = f"{PATH_TO_PLOT}/curr",
+    plot_name = "c) {}".format(r"$(J_{i,z}-J_{z0})/J_0$"),
+    limits = lims,
+    labels = labels,
+    cmap = cmap,
+    hide_xlabels = False,
+    hide_ylabels = False,
+    major_loc = major_loc,
+    minor_loc = minor_loc,
+    levels = (-0.01, 0.01),
+    cbar_label = "",
+    cbar_size=cbar_size,
+    cbar_pad=cbar_pad,
 )
 
 # Attach plot parameters to quantities
@@ -103,9 +137,35 @@ Ey.plot_params.plot_name = "b) {}".format(r"$(E_y-E_{y0})/E_0$")
 Ey.plot_params.hide_xlabels = True
 Ey.plot_params.labels[0] = ""
 
+Jiz.plot_params = deepcopy(curr_plot_params)
+Jix.plot_params = deepcopy(curr_plot_params)
+Jix.plot_params.levels = (-0.2,0.2)
+Jix.plot_params.plot_name = "a) {}".format(r"$(J_{i,x}-J_{x0})/J_0$")
+Jix.plot_params.hide_xlabels = True
+Jix.plot_params.labels[0] = ""
+Jiy.plot_params = deepcopy(curr_plot_params)
+Jiy.plot_params.plot_name = "b) {}".format(r"$(J_{i,y}-J_{y0})/J_0$")
+Jiy.plot_params.hide_xlabels = True
+Jiy.plot_params.labels[0] = ""
+
+curr_plot_params.fig_name = "step_ele"
+curr_plot_params.plot_name = "a) {}".format(r"$(J_{e,z}-J_{z0})/J_0$")
+Jez.plot_params = deepcopy(curr_plot_params)
+Jex.plot_params = deepcopy(curr_plot_params)
+Jex.plot_params.levels = (-0.2,0.2)
+Jex.plot_params.plot_name = "a) {}".format(r"$(J_{e,x}-J_{x0})/J_0$")
+Jex.plot_params.hide_xlabels = True
+Jex.plot_params.labels[0] = ""
+Jey.plot_params = deepcopy(curr_plot_params)
+Jey.plot_params.plot_name = "b) {}".format(r"$(J_{e,y}-J_{y0})/J_0$")
+Jey.plot_params.hide_xlabels = True
+Jey.plot_params.labels[0] = ""
+
 # Create a dictionary containing all maps
 maps_linear_dict = {
     "dens": [Ni, Ne],
     "bfield": [Bx, By, Bz],
     "efield": [Ex, Ey, Ez],
+    "curr_ion": [Jix, Jiy, Jiz],
+    "curr_ele": [Jex, Jey, Jez]
 }
